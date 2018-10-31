120d5e61a5a9bce455fd7cc6ee5b217a23de569c
David <daithilang@hotmail.com>
2018-10-31 15:36:20 +0000

    "Fixed Import"

--- src/main/java/com/mycompany/softeng3ass1partb/PartB.java
+++ src/main/java/com/mycompany/softeng3ass1partb/PartB.java
@@ -19,9 +19,11 @@ public class PartB {
         ClassOf2019[0].setName("David Langley");
         ClassOf2019[0].setId("15340201");
         ClassOf2019[0].setDateOfBirth(new LocalDate(1996, 10, 10));
+        
         ClassOf2019[1].setName("Lorcan Barrett-Gomez");
         ClassOf2019[1].setId("17329883");
         ClassOf2019[1].setDateOfBirth(new LocalDate(1998, 3, 15));
+        
         ClassOf2019[2].setName("Colin Delahunt");
         ClassOf2019[2].setId("15402918");
         ClassOf2019[2].setDateOfBirth(new LocalDate(1996, 12, 23));
@@ -30,9 +32,11 @@ public class PartB {
         diffModules[0].setName("Programming 1");
         diffModules[0].setId("CT101");
         diffModules[0].addStudents(ClassOf2019[0]);
+        
         diffModules[1].setName("History");
         diffModules[1].setId("HS101");
         diffModules[1].addStudents(ClassOf2019[1]);
+        
         diffModules[2].setName("Biology");
         diffModules[2].setId("BY101");
         diffModules[2].addStudents(ClassOf2019[2]);
@@ -42,5 +46,15 @@ public class PartB {
         courses[0].setsDate(new LocalDate(2015,9,1));
         courses[0].seteDate(new LocalDate(2019,5,10));
         courses[0].addModules(diffModules[0]);
+   
+        courses[1].setName("Arts");
+        courses[1].setsDate(new LocalDate(2016,9,1));
+        courses[1].seteDate(new LocalDate(2020,5,10));
+        courses[1].addModules(diffModules[1]);
+        
+        courses[2].setName("Environmental Science");
+        courses[2].setsDate(new LocalDate(2017,9,1));
+        courses[2].seteDate(new LocalDate(2021,5,10));
+        courses[2].addModules(diffModules[2]);
    }
 }
